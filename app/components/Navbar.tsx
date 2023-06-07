362d030538953b44e92f4fe29d0e3efda73c0655
Thomas <tombutler4@hotmail.com>
2023-06-07 01:01:12 -0500

    basic nav bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import React from 'react';
 
+// TODO: mobile menu
 
 export default function Navbar() {
-
     return (
-        <div>
-
+        <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+            <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
+                <Link href='/'>
+                    <h1 className="text-3xl font-bold">ETH Chicago</h1>
+                </Link>
+                <ul className="hidden sm:flex">
+                    <li className="p-4">
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li className="p-4">
+                        <Link href='/'>Forum</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-   <div className="flex items-center justify-center mt-10 h-screen bg-scroll bg-center bg-cover bg-homepage_bg"> 
+   <div className="flex items-center justify-center h-screen bg-scroll bg-center bg-cover bg-homepage_bg"> 
     <div className="bg-black p-5 rounded-md">
       <h1 className="text-5xl text-white font-bold">ETH Chicago</h1>
     </div> 
