0d9646f532bb12f7c73315cec4b18ee4a41af06e
Thomas <tombutler4@hotmail.com>
2023-06-07 13:11:38 -0500

    basic footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+    return (
+        <div className="bg-black text-white p-5">
+            <h3 className="text-center">Created by Chicagoans, for everyone.</h3>
+            <h3 className="text-center">2023</h3>
+        </div>
+    )
+}
