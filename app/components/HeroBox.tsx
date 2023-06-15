c155dcbcf19c14c36f6d34e2250c3b17d622a3bb
Thomas <tombutler4@hotmail.com>
2023-06-15 10:00:37 -0500

    basic embed

---
+++ app/components/HeroBox.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function HeroBox() {
+    return (
+            <div className="bg-black p-10 rounded-xl items-center max-w-xl">
+                <div className="mb-10 flex justify-center">
+                    <Image 
+                        src="/images/ethchi_banner.jpg" 
+                        alt="ethchi banner" 
+                        width={200} 
+                        height={160}
+                    />
+                </div>
+                <h2 className="text-xl text-white">
+                    Three days of building bridges between web3 builders, creators, traditional businesses, financial experts, and policymakers.
+                </h2>
+                <h1 className="text-3xl text-white text-center mt-10 font-bold">September 15-17, 2023</h1>
+                <ul className="flex justify-center p-10">
+                    <li> 
+                        <HeroButton 
+                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoAtt" 
+                            label="I want to attend"
+                        />
+                    </li>
+                    <li> 
+                        <HeroButton 
+                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoVol" 
+                            label="I want to volunteer"
+                        />
+                    </li>
+                    <li> 
+                        <HeroButton 
+                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoPart" 
+                            label="I want to partner"
+                        />
+                    </li>
+                </ul>
+            </div> 
+    )
+}
+
+
+interface IHeroButton {
+    url: string,
+    label: string
+}
+function HeroButton({ url, label }: IHeroButton) {
+    return (
+        <div>
+            <Link href={url} >
+                <button className="w-32 text-white p-2 border border-white m-2 hover:bg-white hover:text-black font-bold">
+                    {label}
+                </button>
+            </Link>
+        </div>
+    ) 
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
+import VennityMap from "./components/VennityMap";
+import HeroBox from "./components/HeroBox";
 
 export default function Home() {
     return (
-        <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover bg-homepage_bg"> 
-            <div className="bg-black p-10 mt-20 rounded-xl flex-col items-center max-w-xl">
-                <div className="mb-10 flex justify-center">
-                    <Image 
-                        src="/images/ethchi_banner.jpg" 
-                        alt="ethchi banner" 
-                        width={200} 
-                        height={160}
-                    />
-                </div>
-                <h2 className="text-xl text-white">Three days of building bridges between web3 builders, creators, traditional businesses, financial experts, and policymakers.</h2>
-                <h1 className="text-3xl text-white text-center mt-10 font-bold">September 15-17, 2023</h1>
-                <ul className="flex justify-center p-10">
-                    <li> 
-                        <HeroButton 
-                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoAtt" 
-                            label="I want to attend"
-                        />
-                    </li>
-                    <li> 
-                        <HeroButton 
-                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoVol" 
-                            label="I want to volunteer"
-                        />
-                    </li>
-                    <li> 
-                        <HeroButton 
-                            url="https://7tjiyjbn20b.typeform.com/ETHChicagoPart" 
-                            label="I want to partner"
-                        />
-                    </li>
-                </ul>
-            </div> 
+        <div className="flex justify-center items-center h-screen bg-fixed bg-center bg-cover bg-homepage_bg"> 
+
+            <div className="p-5 space-y-20 flex flex-col items-center">
+                <HeroBox />
+                <VennityMap />
+            </div>
+
         </div>
     )
-}
-
-interface IHeroButton {
-    url: string,
-    label: string
-}
-function HeroButton({ url, label }: IHeroButton) {
-    return (
-        <div className="flex ">
-            <Link href={url} >
-                <button className="w-32 text-white p-2 border border-white m-2 hover:bg-white hover:text-black font-bold">
-                    {label}
-                </button>
-            </Link>
-        </div>
-    ) 
 }
