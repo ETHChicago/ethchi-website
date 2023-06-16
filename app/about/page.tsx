855871810106437c51ca2a7b15f74505f6019a83
Thomas <tombutler4@hotmail.com>
2023-06-15 22:30:57 -0500

    updated footer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
 export default function About() {
     return (
-        <div></div>
+        <div>
+
+        </div>
     )
 }
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,86 @@
+import Image from "next/image"
+import Link from "next/link"
+import { FaTelegram, FaTwitter } from "react-icons/fa"
 
 export default function Footer() {
     return (
         <div className="bg-black text-white p-5">
-            <h3 className="text-center">Created by Chicagoans, for everyone.</h3>
-            <h3 className="text-center">2023</h3>
+
+            <div className="m-10 flex flex-row justify-between">
+
+                <div className="m-5">
+                    <Image 
+                        src="/images/ethchi_logo_vertical_white.png"
+                        alt="ethchi logo"
+                        width={200}
+                        height={200}
+                    />
+                </div>
+
+                <div className="m-10">
+                    <h3 className="font-bold">extra links</h3>
+                    <ul>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                    </ul>
+                </div>
+
+                <div className="m-10">
+                    <h3 className="font-bold">extra links</h3>
+                    <ul>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                    </ul>
+                </div>
+
+                <div className="m-10">
+                    <h3 className="font-bold">extra links</h3>
+                    <ul>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                        <Link href="">
+                            <li className="hover:underline">link1</li>
+                        </Link>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="flex flex-row space-x-10 justify-center">
+                <FaTelegram size={40}/>
+                <FaTwitter size={40}/>
+            </div>
+
+            <div className="m-10">
+                <h3 className="text-center">Created by Chicagoans, for everyone.</h3>
+            </div>
         </div>
     )
 }
