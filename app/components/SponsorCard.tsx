0d48ea68d716c7688710ee16694f0da0d3deb93f
Thomas <tombutler4@hotmail.com>
2023-06-16 16:54:42 -0500

    added shadow to homepage components

--- app/components/SponsorCard.tsx
+++ app/components/SponsorCard.tsx
@@ -8,7 +8,7 @@ interface ISponsorCard {
 export default function SponsorCard({logo}: ISponsorCard) {
     return (
             <Link href="https://arbitrum.io/">
-                <div className="flex flex-col justify-center items-center bg-white p-2 w-70 m-4 rounded-xl outline hover:bg-neutral-900 ease-in duration-100">
+                <div className="flex flex-col justify-center items-center bg-white p-2 w-70 m-4 rounded-xl outline hover:bg-neutral-900 ease-in duration-100 shadow-2xl">
                     <Image
                         src={logo}
                         alt="sponsor logo"
