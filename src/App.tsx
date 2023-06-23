9698d804290d06a82da701928a14ad98194e76fe
Thomas <tombutler4@hotmail.com>
2023-06-22 21:30:08 -0500

    basic ticket checkout popup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import About from './pages/About';
 import Home from './pages/Home';
+import Tickets from './pages/Tickets';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/tickets" element={<Tickets />} />
       </Routes>
 
       <Footer />
