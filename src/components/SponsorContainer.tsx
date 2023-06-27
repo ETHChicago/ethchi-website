34f1b8025dd3740947ad77cbb22e494107b47063
Thomas <tombutler4@hotmail.com>
2023-06-26 22:18:14 -0500

    dynamic stlying for sponsor logos

--- src/components/SponsorContainer.tsx
+++ src/components/SponsorContainer.tsx
@@ -1,7 +1,8 @@
 
 export default function SponsorContainer() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-4">
+        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-4">
             <SponsorCard logo="/images/logos/ARBITRUM.png" url="https://arbitrum.io/"/>
             <SponsorCard logo="/images/logos/BUIDLGUIDL.jpeg" url="https://buidlguidl.com/"/>
             <SponsorCard logo="/images/logos/CREATIVE_MORNINGS.png" url="https://creativemornings.com/cities/CHI"/>
@@ -17,8 +18,16 @@ export default function SponsorContainer() {
             <SponsorCard logo="/images/logos/rtl.jpg" url=""/>
             <SponsorCard logo="/images/logos/RUNTIME_VERIFICATION.png" url="https://runtimeverification.com/"/>
             <SponsorCard logo="/images/logos/vennity.png" url="https://www.vennity.co/"/>
-            <SponsorCard logo="/images/logos/WORLD_BUSINESS_CHICAGO.png" url="https://worldbusinesschicago.com/"/>
+            <div className="lg:hidden w-full h-full">
+                <SponsorCard logo="/images/logos/WORLD_BUSINESS_CHICAGO.png" url="https://worldbusinesschicago.com/"/>
+            </div>
         </div>
+        <div className="hidden lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-4">
+            <div className="w-full h-full" />
+            <SponsorCard logo="/images/logos/WORLD_BUSINESS_CHICAGO.png" url="https://worldbusinesschicago.com/"/>
+            <div className="w-full h-full" />
+        </div>
+        </>
     )
 }
 
@@ -29,7 +38,7 @@ interface ISponsorCard {
 function SponsorCard({logo, url}: ISponsorCard) {
     return (
             <a href={url}>
-                <div className="flex justify-center items-center bg-transparent p-2 w-70 m-4 rounded-xl outline hover:bg-neutral-900 ease-in duration-100 shadow-2xl">
+                <div className="flex justify-center items-center h-full w-full bg-transparent p-2 rounded-xl outline hover:bg-neutral-900 ease-in duration-100 shadow-2xl">
                     <img
                         src={logo}
                         alt="sponsor logo"
