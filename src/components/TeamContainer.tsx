7fd6b39f673077640027a87fd1797f7b63938f8d
Thomas <tombutler4@hotmail.com>
2023-06-28 15:54:34 -0500

    removed duplicate team cards

--- src/components/TeamContainer.tsx
+++ src/components/TeamContainer.tsx
@@ -2,28 +2,24 @@
 export default function TeamContainer() {
     return (
         <div className="felx items-center justify-center">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 place-items-center">
-            <CoreTeamCard name="Andrew Poterala" title="Summoner/Member at ChainVision" img="/logo.png"/>
-            <CoreTeamCard name="James He" title="Ethereum Core Developer at Offchain Labs" img="/logo.png"/>
-            <CoreTeamCard name="Andrew Cashen" title="Crypto Art Curator and Special Event Producer" img="/images/team/cashen.jpg"/>
-            <CoreTeamCard name="Bobbie Rachford" title="Blockchain Community Growth Leader" img="/logo.png"/>
-        </div>
-        <div className="hidden md:grid grid-cols-3 gap-4 place-items-center">
-            <CoreTeamCard name="Seth Akkerman" title="Interactive Designer" img="/logo.png"/>
-            <CoreTeamCard name="Cami Darling" title="Web3 Marketing and Content Manager" img="/logo.png"/>
-            <CoreTeamCard name="Thomas Butler" title="Freelance Software Engineer" img="/images/team/butler.jpg"/>
-        </div>
-        <div className="md:hidden grid grid-cols-2 gap-4 place-items-center">
-            <CoreTeamCard name="Seth Akkerman" title="Interactive Designer" img="/logo.png"/>
-            <CoreTeamCard name="Cami Darling" title="Web3 Marketing and Content Manager" img="/logo.png"/>
-        </div>
-        <div className="md:hidden grid grid-cols-2 gap-4 place-items-center">
-            <CoreTeamCard name="Seth Akkerman" title="Interactive Designer" img="/logo.png"/>
-            <CoreTeamCard name="Cami Darling" title="Web3 Marketing and Content Manager" img="/logo.png"/>
-        </div>
-        <div className="md:hidden grid grid-cols-1 gap-4 place-items-center">
-            <CoreTeamCard name="Thomas Butler" title="Freelance Software Engineer" img="/images/team/butler.jpg"/>
-        </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 place-items-center">
+                <CoreTeamCard name="Andrew Poterala" title="Summoner/Member at ChainVision" img="/logo.png"/>
+                <CoreTeamCard name="James He" title="Ethereum Core Developer at Offchain Labs" img="/logo.png"/>
+                <CoreTeamCard name="Andrew Cashen" title="Crypto Art Curator and Special Event Producer" img="/images/team/cashen.jpg"/>
+                <CoreTeamCard name="Bobbie Rachford" title="Blockchain Community Growth Leader" img="/logo.png"/>
+            </div>
+            <div className="hidden md:grid grid-cols-3 gap-4 place-items-center">
+                <CoreTeamCard name="Seth Akkerman" title="Interactive Designer" img="/logo.png"/>
+                <CoreTeamCard name="Cami Darling" title="Web3 Marketing and Content Manager" img="/logo.png"/>
+                <CoreTeamCard name="Thomas Butler" title="Freelance Software Engineer" img="/images/team/butler.jpg"/>
+            </div>
+            <div className="md:hidden grid grid-cols-2 gap-4 place-items-center">
+                <CoreTeamCard name="Seth Akkerman" title="Interactive Designer" img="/logo.png"/>
+                <CoreTeamCard name="Cami Darling" title="Web3 Marketing and Content Manager" img="/logo.png"/>
+            </div>
+            <div className="md:hidden grid grid-cols-1 gap-4 place-items-center">
+                <CoreTeamCard name="Thomas Butler" title="Freelance Software Engineer" img="/images/team/butler.jpg"/>
+            </div>
         </div>
     )
 }
