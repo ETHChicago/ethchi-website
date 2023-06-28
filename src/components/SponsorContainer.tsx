77a9ea877662d7e796f2e1f1053ddfc226038a62
Thomas <tombutler4@hotmail.com>
2023-06-28 15:48:50 -0500

    updated sponsor logos

--- src/components/SponsorContainer.tsx
+++ src/components/SponsorContainer.tsx
@@ -2,29 +2,30 @@
 export default function SponsorContainer() {
     return (
         <>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-16">
             <SponsorCard logo="/images/logos/ARBITRUM.png" url="https://arbitrum.io/"/>
-            <SponsorCard logo="/images/logos/BUIDLGUIDL.jpeg" url="https://buidlguidl.com/"/>
-            <SponsorCard logo="/images/logos/CREATIVE_MORNINGS.png" url="https://creativemornings.com/cities/CHI"/>
-            <SponsorCard logo="/images/logos/GE_CHAMBER.png" url="https://www.gechamber.com/"/>
-            <SponsorCard logo="/images/logos/GLOBAL_DCA.png" url="https://global-dca.org/"/>
-            <SponsorCard logo="/images/logos/HOPSCOTCH.jpg" url="https://learn.hopscotch.trade/"/>
-            <SponsorCard logo="/images/logos/icebreaker.png" url="https://www.icebreaker.xyz/"/>
-            <SponsorCard logo="/images/logos/chicagoDAO.png" url="https://chicagodao.io/"/>
-            {/*<SponsorCard logo="/images/logos/JUMP.png" url="https://jumpcrypto.com/"/>*/}
+            <SponsorCard logo="/images/logos/BUIDLGUIDL.png" url="https://buidlguidl.com/"/>
+            <SponsorCard logo="/images/logos/CREATIVE MORN.png" url="https://creativemornings.com/cities/CHI"/>
+            <SponsorCard logo="/images/logos/ECC.png" url="https://www.gechamber.com/"/>
+            <SponsorCard logo="/images/logos/GLOBAL DCA.png" url="https://global-dca.org/"/>
+            <SponsorCard logo="/images/logos/HOPSCOTCH.png" url="https://learn.hopscotch.trade/"/>
+            <SponsorCard logo="/images/logos/ICEBREAKER.png" url="https://www.icebreaker.xyz/"/>
+            <SponsorCard logo="/images/logos/CHICAGO DAO.png" url="https://chicagodao.io/"/>
             <SponsorCard logo="/images/logos/LEDGIBLE.png" url="https://ledgible.io/"/>
             <SponsorCard logo="/images/logos/MANTLE.png" url="https://www.mantle-labs.com/"/>
-            <SponsorCard logo="/images/logos/michiganBlockchain.png" url=""/>
-            <SponsorCard logo="/images/logos/rtl.jpg" url=""/>
-            <SponsorCard logo="/images/logos/RUNTIME_VERIFICATION.png" url="https://runtimeverification.com/"/>
-            <SponsorCard logo="/images/logos/vennity.png" url="https://www.vennity.co/"/>
-            <div className="md:hidden lg:block w-full h-full">
-                <SponsorCard logo="/images/logos/WORLD_BUSINESS_CHICAGO.png" url="https://worldbusinesschicago.com/"/>
+            <SponsorCard logo="/images/logos/UMICH BCHAIN.png" url=""/>
+            <SponsorCard logo="/images/logos/RUNTIME VERIFICATION.png" url="https://runtimeverification.com/"/>
+            <div className="lg:hidden">
+                <SponsorCard logo="/images/logos/VENNITY.png" url="https://www.vennity.co/"/>
+            </div>
+            <div className="lg:hidden">
+                <SponsorCard logo="/images/logos/WBC.png" url="https://worldbusinesschicago.com/"/>
             </div>
         </div>
-        <div className="hidden md:grid lg:hidden grid-cols-1 gap-6 mx-64">
+        <div className="hidden md:hidden lg:grid grid-cols-4 mx-64">
             <div className="w-full h-full" />
-            <SponsorCard logo="/images/logos/WORLD_BUSINESS_CHICAGO.png" url="https://worldbusinesschicago.com/"/>
+            <SponsorCard logo="/images/logos/WBC.png" url="https://worldbusinesschicago.com/"/>
+            <SponsorCard logo="/images/logos/VENNITY.png" url="https://www.vennity.co/"/>
             <div className="w-full h-full" />
         </div>
         </>
@@ -38,7 +39,7 @@ interface ISponsorCard {
 function SponsorCard({logo, url}: ISponsorCard) {
     return (
             <a href={url}>
-                <div className="flex justify-center items-center h-full w-full bg-transparent p-2 rounded-xl outline hover:bg-secondaryBrand ease-in duration-100 shadow-2xl">
+                <div className="flex justify-center items-center h-full w-full bg-transparent hover:bg-secondaryBrand hover:outline rounded-xl hover:shadow-xl ease-in duration-100">
                     <img
                         src={logo}
                         alt="sponsor logo"
