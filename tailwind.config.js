fc2e566066efe97cb13a76c8a13a9aa2b29bcb44
Thomas <trbiv@proton.me>
2023-07-27 21:22:12 -0500

    background points to high res link

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'homepage_bg': "url('../public/images/willis_tower_bg.jpg')",
+        'homepage_bg': "url('https://images.unsplash.com/photo-1509653087866-91f6c2ab59f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1970&q=80')",
         'ticket_page_bg': "url('../public/images/ethchi_flag_big.jpg')",
       }
     },
