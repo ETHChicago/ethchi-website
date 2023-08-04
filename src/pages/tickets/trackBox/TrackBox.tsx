73ce05c111860a111741f595a1c46223094ee092
Thomas <trbiv@proton.me>
2023-08-03 20:04:37 -0500

    updated ticket box

--- src/pages/tickets/trackBox/TrackBox.tsx
+++ src/pages/tickets/trackBox/TrackBox.tsx
@@ -7,10 +7,10 @@ interface ITrackBox {
 }
 export default function TrackBox({trackName, trackDescription, applicationLink, checkoutText, checkoutPopup }: ITrackBox) {
     return (
-        <div className="flex flex-col justify-between m-10 p-4 shadow-xl w-80 h-96 max-w-xl bg-primaryBackground text-primaryType">
+        <div className="flex flex-col justify-between m-10 p-4 shadow-xl w-96 h-[550px] md:h-[650px] lg:h-[500px] max-w-xl bg-primaryBackground text-primaryType">
             <div>
                 <h1 className="text-2xl text-primaryType font-mono font-bold">{trackName}</h1>
-                <div className="my-2">
+                <div className="mt-10 my-2">
                     <h1 className="text-md text-primaryType font-mono font-bold">Perks:</h1>
                     <ul className="ml-6 text-sm list-disc marker:text-primaryType">
                         {
@@ -25,7 +25,9 @@ export default function TrackBox({trackName, trackDescription, applicationLink, 
             <div className="flex justify-center items-center m-4">
                 {applicationLink && (
                     <a href={applicationLink} target="_blank" rel="noreferrer" >
-                        <button className="outline outline-2 rounded-md p-2 text-primaryType bg-primaryBackground hover:bg-primaryBrand ease-in duration-100">{checkoutText}</button>
+                        <button className="outline outline-2 rounded-md p-2 text-primaryType bg-primaryBackground hover:bg-primaryBrand ease-in duration-100">
+                            {checkoutText}
+                        </button>
                     </a>
                 )}
                 {checkoutPopup && (
