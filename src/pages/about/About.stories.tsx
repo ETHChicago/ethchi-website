36125f96a700994083fca141d55242b254ed5c6a
Thomas <trbiv@proton.me>
2023-08-07 16:00:25 -0500

    updated about page story

--- src/pages/about/About.stories.tsx
+++ src/pages/about/About.stories.tsx
@@ -3,6 +3,8 @@ import { HashRouter} from 'react-router-dom'
 import About from './About'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import { WagmiConfig } from 'wagmi'
+import wagmiConfig from '../../wagmiConfig'
 
 const meta: Meta<typeof About> = {
     title: 'Pages/About/About Page',
@@ -15,10 +17,12 @@ type Story = StoryObj<typeof About>
 
 export const Primary: Story = {
     render: () => (
-        <HashRouter>
-            <Navbar />
-            <About />
-            <Footer />
-        </HashRouter>
+        <WagmiConfig config={wagmiConfig}>
+            <HashRouter>
+                <Navbar />
+                <About />
+                <Footer />
+            </HashRouter>
+        </WagmiConfig>
     )
 }
