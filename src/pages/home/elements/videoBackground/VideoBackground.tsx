c3e3e509bd46caa75a91783c0292bce5cd213440
Thomas <trbiv@proton.me>
2023-08-14 17:56:18 -0500

    centered video

--- src/pages/home/elements/videoBackground/VideoBackground.tsx
+++ src/pages/home/elements/videoBackground/VideoBackground.tsx
@@ -2,7 +2,7 @@
 export default function VideoBackground({videoId}: {videoId: string}) {
 
     return (
-        <div className="flex w-full h-full">
+        <div className="flex w-full h-full justify-center items-center">
           <video autoPlay loop muted>
             <source src="/videos/ethchi-hype-video.mp4" type='video/mp4' />
           </video>
