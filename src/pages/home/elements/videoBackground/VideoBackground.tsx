1309648ccbda93d549988d45e7f6c9e28d51d73f
Thomas <trbiv@proton.me>
2023-08-14 18:59:22 -0500

    updates to video background

--- src/pages/home/elements/videoBackground/VideoBackground.tsx
+++ src/pages/home/elements/videoBackground/VideoBackground.tsx
@@ -3,7 +3,7 @@ export default function VideoBackground({videoId}: {videoId: string}) {
 
     return (
         <div className="flex w-full h-full justify-center items-center">
-          <video autoPlay loop muted>
+          <video autoPlay loop muted playsInline>
             <source src="/videos/ethchi-hype-video.mp4" type='video/mp4' />
           </video>
         </div>
