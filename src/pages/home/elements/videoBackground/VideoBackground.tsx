94d59b4e2d91bd6b159e52bc01d6fe02ec6912da
Thomas <trbiv@proton.me>
2023-08-14 17:50:09 -0500

    added video bg

--- src/pages/home/elements/videoBackground/VideoBackground.tsx
+++ src/pages/home/elements/videoBackground/VideoBackground.tsx
@@ -2,29 +2,10 @@
 export default function VideoBackground({videoId}: {videoId: string}) {
 
     return (
-        <div className="flex w-full h-screen">
-          <div className="top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className="text-primaryType text-center">
-
-              <p className="text-primaryType text-xl">Septemper 15-17 2023</p>
-
-              <div className='flex items-center justify-center'>
-                  <img 
-                    src="/images/brand/ethchi_banner.svg"
-                    alt="ETHChi Banner"
-                    width={400}
-                    height={160}
-                    loading="lazy"
-                  />
-              </div>
-
-              <p className="text-primaryType text-xl m-6">
-                THREE DAYS OF BUILDING BRIDGES BETWEEN WEB3 BUILDERS, CREATORS, TRADITIONAL BUSINESSES,
-                FINANCIAL EXPERTS AND POLICYMAKERS.
-              </p>
-
-            </div>
-          </div>
+        <div className="flex w-full h-full">
+          <video autoPlay loop muted>
+            <source src="/videos/ethchi-hype-video.mp4" type='video/mp4' />
+          </video>
         </div>
     )
 }
