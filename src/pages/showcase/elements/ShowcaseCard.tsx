5e632b594434bbac424ecdab036dd582045034eb
TRBIV <trbiv@proton.me>
2023-09-12 14:52:47 -0500

    added content to the showcase

--- src/pages/showcase/elements/ShowcaseCard.tsx
+++ src/pages/showcase/elements/ShowcaseCard.tsx
@@ -1,15 +1,20 @@
-import { ReactNode } from "react"
 
-export default function ShowcaseCard({ side, title, description, children}: {side: string, title: string, description: string, children: ReactNode}) {
+export default function ShowcaseCard({ side, title, description, imageURL, contentURL }: {side: string, title: string, description: string, imageURL: string, contentURL: string}) {
     return (
+        <a href={contentURL} target="_blank" rel="noreferrer">
         <div className={side === 'left' ? "flex w-full justify-start" : "flex w-full justify-end"} >
-           <div className="bg-primaryBackground m-12 p-4 w-[300px] md:w-[500px] lg:w-[600px] shadow-xl rounded-md">
+           <div className="bg-primaryBackground m-12 p-4 w-[300px] md:w-[500px] lg:w-[800px] shadow-xl rounded-md hover:opacity-90">
                 <p className="text-primaryType text-xl font-bold text-center">{title}</p>
                 <div className="flex justify-center items-center p-6">
-                    {children}
+                    <img 
+                        src={imageURL}
+                        alt="showcase" 
+                        width="full"
+                    />
                 </div>
                 <p className="text-primaryType text-center">{description}</p>
             </div> 
         </div>
+        </a>
     )
 }
