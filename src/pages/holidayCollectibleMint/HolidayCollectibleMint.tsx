9c6266509d1bae536faa4d279035bf1371644631
Thomas Butler <trbiv@proton.me>
2023-12-07 21:09:35 -0600

    added simple mint button

--- src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
+++ src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
@@ -1,8 +1,14 @@
 
 export default function HolidayCollectibleMint() {
+    const handleMint = () => {
+        console.log("minting nft")
+    }
+
     return (
-        <div>
-            Holiday Collectible Mint Page
+        <div className="flex justify-center items-center p-10">
+            <button className="bg-primaryBackground p-2 rounded-xl text-primaryType" onClick={handleMint}>
+                Mint Button
+            </button>
         </div>
     )
 }
