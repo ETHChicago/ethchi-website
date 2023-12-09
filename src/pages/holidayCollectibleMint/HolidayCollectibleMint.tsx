f47750a8dd1799397d60c9f975364ca22865deb1
Thomas Butler <trbiv@proton.me>
2023-12-08 21:01:31 -0600

    sitch to sepolia on mint with other network connected

--- src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
+++ src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
@@ -1,31 +1,31 @@
 import { useEffect } from "react"
-import { useAccount, useWaitForTransaction } from "wagmi"
+import { useNetwork, useAccount, useWaitForTransaction, useSwitchNetwork } from "wagmi"
 import { useHolidayCollectible2023SafeMint, usePrepareHolidayCollectible2023SafeMint } from "../../generated/wagmi-hooks"
 import { contracts } from "../../data/contracts"
 
 
 export default function HolidayCollectibleMint() {
     const { address } = useAccount()
-
-
+    const { chain } = useNetwork()
+    const { switchNetwork } = useSwitchNetwork()
     const { data, write } = useHolidayCollectible2023SafeMint({
         address: contracts.holidayCollectible2023 as `0x${string}`,
         args: [address ? address : '0x', '1']
     })
-
     const { isLoading, isSuccess } = useWaitForTransaction({
         hash: data?.hash
     })
 
     
     const handleMint = () => {
-        if (address) {            
-            console.log(`using: ${address}`)
-        } else {
-            console.log("TODO: handle not connected")
+        if (!address) {            
             return
         }
-
+        if (chain?.name != "Sepolia") {
+            switchNetwork && switchNetwork(11155111)
+            return
+        }
+        // write mint transaction
         write()
     }
 
