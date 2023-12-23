a0880dc63a3792e7a59b00762b3b117eef670f25
Thomas Butler <trbiv@proton.me>
2023-12-23 17:42:14 -0500

    updated collectible sizing

--- src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
+++ src/pages/holidayCollectibleMint/HolidayCollectibleMint.tsx
@@ -5,7 +5,7 @@ export default function HolidayCollectibleMint() {
     return (
         <div className="bg-holiday_bg bg-cover bg-no-repeat  min-h-[1200px] flex flex-col justify-center items-center">
 
-            <div className="w-1/2 h-1/2 flex flex-col justify-center items-center p-10 m-10 bg-primaryBackground shadow-xl rounded-xl">
+            <div className="w-1/3 h-1/3 flex flex-col justify-center items-center p-10 m-10 bg-primaryBackground shadow-xl rounded-xl">
                 <video autoPlay loop src="https://ipfs.io/ipfs/bafybeiggtzavg43u27kvsgncrarg5preklzfzahq65lyom5cesouipakpa" />
                 <div className="flex flex-col text-center p-4">
                     <a className="text-white text-xl hover:underline" href="https://www.twinrafters.com/">Art: Twinrafters</a>
