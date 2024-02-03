d1d6c0678676ccff1e661c344dcae23811987c3e
Thomas Butler <trbiv@proton.me>
2024-02-03 10:33:24 -0600

    updated team page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,25 +1,21 @@
-import AboutTextBox from "./elements/aboutTextBox/AboutTextBox"
-import CoreTeamContainer from "./elements/coreTeamContainer/CoreTeamContainer"
-import VolunteerContainer from "./elements/volunteerContainer/VolunteerContainer"
+import AboutTextBox from "./elements/aboutTextBox/AboutTextBox";
+import CoreTeamContainer from "./elements/coreTeamContainer/CoreTeamContainer";
+import VolunteerContainer from "./elements/volunteerContainer/VolunteerContainer";
 
 export default function About() {
+  return (
+    <>
+      {/* about summary section */}
+      <div className="bg-grid_bg_navy w-full h-full">
+        <div className="p-8 lg:p-28">
+          <AboutTextBox />
+        </div>
+      </div>
 
-    return(
-        <>
-            {/* about summary section */}
-            <div className="bg-grid_bg_navy w-full h-full">
-                <div className="p-8 lg:p-28">
-                    <AboutTextBox />
-                </div>
-            </div>
-
-            {/* contributers section */}
-            <div className="bg-primaryBrand w-full h-full">
-                <CoreTeamContainer />
-            </div>
-            <div className="bg-primaryBackground">
-                <VolunteerContainer />
-            </div>
-        </>
-    )
+      {/* contributers section */}
+      <div className="bg-primaryBrand w-full h-full">
+        <CoreTeamContainer />
+      </div>
+    </>
+  );
 }
