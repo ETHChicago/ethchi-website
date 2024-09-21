b1250b7e44ee348c6f91ad777ecaf07b2a0e2526
Thomas <trbiv@proton.me>
2024-09-21 18:59:01 -0500

    update partners

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,180 +1,28 @@
 const partners = {
-  arbitrum: {
-    logo: "/images/logos/ARB_lockup_white.svg",
-    link: "https://arbitrum.io/",
-  },
-  buidlguidl: {
-    logo: "/images/logos/BUIDLGUIDL.jpeg",
-    link: "https://buidlguidl.com/",
+  _1871: {
+    logo: "images/logos/1871_overlay_logo.png",
+    link: "https://1871.com/",
   },
   chainvision: {
     logo: "images/logos/CHAINVISION.png",
     link: "",
   },
-  ge_chamber: {
-    logo: "/images/logos/GE_CHAMBER.png",
-    link: "https://www.gechamber.com/",
-  },
-  globalDCA: {
-    logo: "/images/logos/GLOBAL_DCA.png",
-    link: "https://global-dca.org/",
-  },
-  hopscotch: {
-    logo: "/images/logos/HOPSCOTCH.jpg",
-    link: "https://learn.hopscotch.trade/",
-  },
-  icebreaker: {
-    logo: "/images/logos/icebreaker.png",
-    link: "https://www.icebreaker.xyz/",
-  },
-  chicagoDAO: {
-    logo: "images/logos/chicagoDAO.png",
-    link: "https://chicagodao.io/",
-  },
-  ledgible: {
-    logo: "/images/logos/LEDGIBLE.png",
-    link: "https://ledgible.io/",
-  },
-  mantle: {
-    logo: "/images/logos/MANTLE.png",
-    link: "https://www.mantle-labs.com/",
-  },
-  umich_blockchain: {
-    logo: "/images/logos/umich-bchain.png",
-    link: "https://www.michiganblockchain.org/",
-  },
   rtl: {
     logo: "/images/logos/RTL.jpg",
     link: "https://rosariotechlaw.com/",
-  },
-  holaplex: {
-    logo: "/images/logos/holaplex.png",
-    link: "https://www.holaplex.com/",
-  },
-  runtime_verification: {
-    logo: "/images/logos/RUNTIME_VERIFICATION.png",
-    link: "https://runtimeverification.com/",
   },
   vennity: {
     logo: "/images/logos/vennity.png",
     link: "https://www.vennity.co/",
   },
-  eth_support_program: {
-    logo: "/images/logos/EF-ESP-logo-white-text.png",
-    link: "https://esp.ethereum.foundation/",
-  },
-  wbc: {
-    logo: "/images/logos/WBC.png",
-    link: "https://worldbusinesschicago.com/",
-  },
-  taikai: {
-    logo: "/images/logos/TAIKAI_Logotype_White.png",
-    link: "https://taikai.network/",
-  },
-  cmt_digital: {
-    logo: "/images/logos/cmt-digital.jpeg",
-    link: "https://cmt.digital/",
-  },
-  formidium: {
-    logo: "/images/logos/formidium.png",
-    link: "https://formidium.com/",
-  },
-  ercx: {
-    logo: "/images/logos/ercx-logo.png",
-    link: "https://ercx.runtimeverification.com/",
-  },
-  c4: {
-    logo: "/images/logos/c4-logo.png",
-    link: "https://cryptoconsortium.org/",
-  },
   im_not_art: {
     logo: "/images/logos/imNotArt.png",
     link: "https://www.imnotart.com/",
-  },
-  alexandria: {
-    logo: "images/logos/alexandria.png",
-    link: "https://alexandrialabs.xyz/",
-  },
-  moonshot_commons: {
-    logo: "images/logos/hackQuest.svg",
-    link: "https://www.hackquest.io/",
-  },
-  api3: {
-    logo: "images/logos/api3.png",
-    link: "https://api3.org/",
-  },
-  travel_swap: {
-    logo: "images/logos/travel-swap.png",
-    link: "https://travelswap.xyz/",
   },
   craft_the_future: {
     logo: "images/logos/craft-the-future.svg",
     link: "https://www.craftthefuture.xyz/",
   },
-  tasty_crypto: {
-    logo: "images/logos/tasty-crypto.jpg",
-    link: "https://www.tastycrypto.com/",
-  },
-  lil_nouns: {
-    logo: "images/logos/lil-nouns.svg",
-    link: "https://lilnouns.wtf/",
-  },
-  crypto_recruiters: {
-    logo: "images/logos/cryptoRecruiters.png",
-    link: "https://thecryptorecruiters.io/",
-  },
-  wormhole: {
-    logo: "images/logos/wormhole.png",
-    link: "https://wormhole.com/",
-  },
-  devreluni: {
-    logo: "images/logos/devreluni.png",
-    link: "https://www.devreluni.com/",
-  },
-  coherence: {
-    logo: "images/logos/coherenceWhite.svg",
-    link: "https://www.coherence.finance/",
-  },
-  allenvendever: {
-    logo: "images/logos/allenvandever.png",
-    link: "https://www.allenvandever.com/",
-  },
-  frankendao: {
-    logo: "images/logos/frankendao.png",
-    link: "https://www.3dfrankenpunks.com/",
-  },
-  amberdata: {
-    logo: "images/logos/amberdata.jpg",
-    link: "https://www.amberdata.io/",
-  },
-  blockadopt: {
-    logo: "images/logos/blockadopt.png",
-    link: "https://blockadopt.xyz/",
-  },
-  herotrainer: {
-    logo: "images/logos/herotrainer.png",
-    link: "https://herotrainer.io/",
-  },
-  cambellteague: {
-   logo: "images/logos/cambell-teague.jpg",
-   link: "https://campbellteague.com/"
-  },
-  node40: {
-    logo: "images/logos/node40.jpg",
-    link: "https://www.node40.com/"
-  },
-  decasonic: {
-    logo: "images/logos/decasonic.png",
-    link: "https://www.decasonic.com/"
-  },
-  ceptorclub: {
-    logo: "images/logos/ceptor-club.png",
-    link: "https://ceptor.club/"
-  },
-  _1871: {
-      logo: "images/logos/1871_overlay_logo.png",
-      link: "https://1871.com/"
-  }
 };
 
 export default partners;
