132c8cf406fda32b7fb233a01c15f68b5ffcaea8
Thomas <trbiv@proton.me>
2025-04-02 21:59:36 -0500

    remove

--- old/src/data/contracts.ts
+++
@@ -1,7 +0,0 @@
-
-export const contracts = {
-    holidayCollectible2023: {
-        sepolia: '0xE7Aa26b165C794995357bEE83002f2BdcEeD659B',
-        mainnet: '0x803424f4D80Ab64cfB5816C70C5c339c52108c50'
-    }
-}
--- old/src/pages/HackerTicketRedirect.tsx
+++
@@ -1,7 +0,0 @@
-import { hackerCheckoutUrl } from "../utils/checkoutConfig"
-
-export default function HackerTicketRedirect() {
-    console.log(`Redirecting to: ${hackerCheckoutUrl}`)
-    window.location.replace(hackerCheckoutUrl)
-    return null
-}
