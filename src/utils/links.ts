7573354309b0392b47a611d50b4fe03f55f8fd11
Thomas <trbiv@proton.me>
2025-04-12 12:39:56 -0500

    adding innovation summit and lab sections

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -3,6 +3,8 @@ const links = {
   events: "https://lu.ma/ethchicago",
   twitter: "https://x.com/0xEthChicago",
   telegram: "https://t.me/+9jKe9bg8FEg2OGMx",
+  summit: "https://1871.com/innovation-labs/emerging-tech-summit/",
+  innovationLab: "https://1871.com/innovation-labs/web3-innovation-lab/",
 };
 
 export default links;
