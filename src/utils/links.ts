f60a56451f12da9639c5bc2e96a8334a013b3ce6
James Carnley <JamesCarnley@gmail.com>
2025-09-10 16:09:34 -0500

    Add Discord to socials

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -2,8 +2,8 @@ const links = {
   newsletter: "https://paragraph.xyz/@ethchicago",
   events: "https://lu.ma/ethchicago",
   twitter: "https://x.com/0xEthChicago",
+  discord: "https://discord.gg/xh9UsBTWVQ",
   telegram: "https://t.me/+9jKe9bg8FEg2OGMx",
-  summit: "https://1871.com/innovation-labs/emerging-tech-summit/",
   innovationLab: "https://1871.com/innovation-labs/web3-innovation-lab/",
 };
 
